6ab5d90f8f06ea1beabb27d211725de85752ddf3
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-15 18:50:21 -0400

    attacking logic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 use bevy::prelude::*;
-use GMTK2022::assetloader::*;
+use GMTK2022::{
+    assetloader::*,
+    game::{GamePlugin, NextTurnEvent, StartRoundEvent},
+    prefab::PrefabPlugin,
+    troop::TroopPlugin,
+};
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins)
         .add_plugin(AssetLoadPlugin)
-        .add_startup_system(setup)
-        .run();
+        .add_startup_system(setup);
+
+    app.add_plugin(GamePlugin)
+        .add_plugin(PrefabPlugin)
+        .add_plugin(TroopPlugin);
+
+    app.add_system(debug);
+
+    app.run();
 }
 
 fn setup(mut commands: Commands, sheets: Res<AssetSheets>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteSheetBundle {
-        sprite: TextureAtlasSprite { ..default() },
-        texture_atlas: sheets.0.get(0).unwrap().clone(),
-        transform: Transform { ..default() },
-        ..default()
-    });
+}
+
+fn debug(
+    keys: Res<Input<KeyCode>>,
+    mut start_round_writer: EventWriter<StartRoundEvent>,
+    mut next_turn_writer: EventWriter<NextTurnEvent>,
+) {
+    if keys.just_pressed(KeyCode::S) {
+        info!("pressed: starting round");
+        start_round_writer.send(StartRoundEvent);
+    }
+    if keys.just_pressed(KeyCode::T) {
+        info!("pressed: next turn");
+        next_turn_writer.send(NextTurnEvent);
+    }
 }
