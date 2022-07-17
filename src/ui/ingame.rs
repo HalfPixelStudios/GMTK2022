35670de40531931ea7cb38fee18d32497b364fd0
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-16 21:56:13 -0400

    stat ui get data from game

--- src/ui/ingame.rs
+++ src/ui/ingame.rs
@@ -1,9 +1,11 @@
-use bevy::{app::*, ecs::prelude::*};
+use bevy::{app::*, ecs::prelude::*, core::Name};
 use kayak_ui::{
     bevy::*,
-    core::{styles::*, *},
+    core::{styles::*, Binding, Color, bind, render, rsx, widget, VecTracker, constructor, Bound, MutableBound},
     widgets::*,
 };
+
+use crate::{troop::{Tag, Stats}, game::Game};
 
 use super::components::stats_board::StatsBoard;
 
@@ -11,32 +13,76 @@ pub struct InGamePlugin;
 
 impl Plugin for InGamePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_startup_system(render_ui);
+        app.add_startup_system(render_ui)
+            .add_system(update_stat_data);
     }
 }
 
+#[derive(Clone, PartialEq)]
+struct StatDataEntry {
+    pub troop_name: String,
+    pub health_percent: f32,
+    pub speed: u32,
+    pub defence: u32,
+}
+
+#[derive(Default, Clone, PartialEq)]
+struct StatData {
+    party: Vec<StatDataEntry>,
+    enemies: Vec<StatDataEntry>,
+}
+
 fn render_ui(mut cmd: Commands) {
+
+    cmd.insert_resource(bind(StatData::default()));
+
     let context = BevyContext::new(|context| {
-        let bg_style = Style {
-            background_color: StyleProp::Value(Color {
-                r: 1.,
-                g: 0.,
-                b: 0.,
-                a: 1.,
-            }),
-            width: StyleProp::Value(Units::Pixels(100.)),
-            height: StyleProp::Value(Units::Pixels(100.)),
-            ..Default::default()
-        };
+
         render! {
             <kayak_ui::widgets::App>
-                <StatsBoard troop_name={"warrior".into()} health_percent={0.75} speed={10} defence={20}></StatsBoard>
+                <StatsList></StatsList>
             </kayak_ui::widgets::App>
         }
     });
     cmd.insert_resource(context);
 }
 
+#[widget]
+fn StatsList() {
+
+    let stat_data_binding = context.query_world::<Res<Binding<StatData>>, _, _>(|stat_data| stat_data.clone());
+    context.bind(&stat_data_binding);
+
+    rsx! {
+        <>
+        {VecTracker::from(stat_data_binding.get().party.iter().map(|data| {
+            constructor! {
+                <StatsBoard troop_name={data.troop_name.clone()} health_percent={data.health_percent} speed={data.speed} defence={data.defence}></StatsBoard>
+            }
+        }))}
+        </>
+    }
+}
+
+
 fn destroy_ui(mut cmd: Commands) {
     cmd.remove_resource::<BevyContext>();
+}
+
+fn update_stat_data(query: Query<(&Name, &Tag, &Stats)>, binding: Res<Binding<StatData>>) {
+     
+    let mut new_stat_data = StatData::default();
+
+    for (name, tag, stats) in query.iter() {
+
+        let health_percent = stats.health() as f32 / stats.base_health() as f32;
+        let stat_entry = StatDataEntry { troop_name: name.into(),  health_percent, speed: stats.speed(), defence: stats.defence() };
+
+        match *tag {
+            Tag::Player => { new_stat_data.party.push(stat_entry); },
+            Tag::Enemy => { new_stat_data.enemies.push(stat_entry); }
+        }
+    }
+
+    binding.set(new_stat_data);
 }
